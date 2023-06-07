6347b5ae0d000d84dc4c020c077bb021fc29c03f
rony <ronyramos495@gmail.com>
2023-06-06 22:49:33 -0500

    algunas modificaionces

--- app/src/main/java/com/romzc/labpaging/database/AppDatabase.kt
+++ app/src/main/java/com/romzc/labpaging/database/AppDatabase.kt
@@ -5,21 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(
-    entities = [CountryEntity::class],
-    version = 2
-)
+@Database(entities = [CountryEntity::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun countryDao(): CountryDao
-
     companion object {
         private var INSTANCE: AppDatabase? = null
-
         fun getInstance(context: Context): AppDatabase {
             synchronized(this) {
                 var instance = INSTANCE
-
-
                 if (instance == null) {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
@@ -29,11 +22,9 @@ abstract class AppDatabase : RoomDatabase() {
                         .addCallback(FillDatabaseWithCountries(context))
                         .build()
                     INSTANCE = instance
-
                 }
                 return instance
             }
-
         }
     }
 }
